0b15c595db22506274dc80c1b6c39eea7a8975ca
Carlos Vega <carlosvega@fb.com>
2022-06-16 10:51:58 -0700

    Functional load more movies button

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ const img_path = 'https://image.tmdb.org/t/p/w500/'
 let page = 1;
 // Query Selectors
 const movieGridElement = document.getElementById('movies-grid');
-
+const loadMoreMoviesElement = document.getElementById('load-more-movies-btn');
 
 async function getData() {
-    let response = await fetch(url_path + '/now_playing?api_key=' + api_key + '&page=' + page.toString()); //
+    let response = await fetch(url_path + '/now_playing?api_key=' + api_key + '&page=' + page.toString());
     let data = await response.json();
     initializeMoviesGrid (data['results']);
 }
 
 function initializeMoviesGrid(movies) {
     movieGridElement.innerHTML = '';
-    movies.forEach(movie => {initializeMovieCard(movie)})
+    movies.forEach(movie => {addMovieCard(movie)})
 }
 
-function initializeMovieCard(movie){
+function addMovieCard(movie){
     movieGridElement.innerHTML += 
     `
-    <div class="movie-card">
+    <div class="movie-card" id='${movie.id}'>
         <img class="movie-poster" src="${img_path + movie.poster_path }" alt="${'Poster for ' + movie.original_title}"></img>
         <span class="movie-title">
             ${movie.original_title}
@@ -30,8 +30,29 @@ function initializeMovieCard(movie){
         </span>
     </div>
     `;
+    console.log(movie.original_title)
+}
+
+function addMoviesGrid(movies) {
+    movies.forEach(movie => {addMovieCard(movie)})
+}
+
+async function getMoreData(){
+    page += 1
+    console.log(page)
+    let response = await fetch(url_path + '/now_playing?api_key=' + api_key + '&page=' + page.toString());
+    let data = await response.json();
+    addMoviesGrid (data['results']);
+}
+
+function addEventListeners(loadMoreMoviesElement) {
+    loadMoreMoviesElement.addEventListener("click", (e) => {        
+        getMoreData()
+      })
+    
 }
 
 window.onload = function () {
     getData();
+    addEventListeners(loadMoreMoviesElement);
 }
