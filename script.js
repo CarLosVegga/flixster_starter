9385ea4cc9da056e07da653f5afec6f1db705dd0
Carlos Vega <carlosvega@fb.com>
2022-06-16 01:33:06 -0700

    Show initial 20 movies with titles, posters and rates

---
+++ script.js
@@ -0,0 +1,37 @@
+const api_key = 'a3016ba6049f537e33cb6a0c27a8a821';
+const url_path = 'https://api.themoviedb.org/3/movie';
+const img_path = 'https://image.tmdb.org/t/p/w500/'
+let page = 1;
+// Query Selectors
+const movieGridElement = document.getElementById('movies-grid');
+
+
+async function getData() {
+    let response = await fetch(url_path + '/now_playing?api_key=' + api_key + '&page=' + page.toString()); //
+    let data = await response.json();
+    initializeMoviesGrid (data['results']);
+}
+
+function initializeMoviesGrid(movies) {
+    movieGridElement.innerHTML = '';
+    movies.forEach(movie => {initializeMovieCard(movie)})
+}
+
+function initializeMovieCard(movie){
+    movieGridElement.innerHTML += 
+    `
+    <div class="movie-card">
+        <img class="movie-poster" src="${img_path + movie.poster_path }" alt="${'Poster for ' + movie.original_title}"></img>
+        <span class="movie-title">
+            ${movie.original_title}
+        </span>
+        <span class="movie-votes">
+            ${movie.vote_average}
+        </span>
+    </div>
+    `;
+}
+
+window.onload = function () {
+    getData();
+}
